b21f2cfc41bf22649df8bdb57ab5ba9867ee4770
neeeerrd <emkelleher@protonmail.com>
2023-10-27 13:04:54 +0100

    add nvim plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -10,7 +10,41 @@ if not vim.loop.fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
-require("lazy").setup(plugins)
-local plugins = {
-  "folke/tokyonight.nvim"
-}
+require("lazy").setup({
+  {	
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- load the colorscheme here
+      vim.cmd([[colorscheme tokyonight-night]])
+    end,
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons"
+    }
+  },
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons"
+    }
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+          ensure_installed = { "c", "lua", "vim" },
+          sync_install = true,
+          highlight = { enable = true },
+          indent = { enable = true },  
+        })
+    end
+  }
+})
+require("lualine").setup()
